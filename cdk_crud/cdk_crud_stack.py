b63a7647dccc1df4d2631be049550ace50247048
Bentley <patbent@a483e701fcdf.ant.amazon.com>
2021-04-03 03:21:47 -0500

    Adds API GW w/ Lambda Back-End

--- cdk_crud/cdk_crud_stack.py
+++ cdk_crud/cdk_crud_stack.py
@@ -1,9 +1,8 @@
 from aws_cdk import (
     aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
-    core
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
+    core,
 )
 
 
@@ -12,13 +11,14 @@ class CdkCrudStack(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "CdkCrudQueue",
-            visibility_timeout=core.Duration.seconds(300),
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.asset('lambda'),
+            handler='hello.handler',
         )
 
-        topic = sns.Topic(
-            self, "CdkCrudTopic"
+        apigw.LambdaRestApi(
+            self, 'Endpoint',
+            handler=my_lambda,
         )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
